ccaa3c1465c3f842bbaad495ab6c9166b954f924
HeeJung <72288033+druerie@users.noreply.github.com>
2023-10-14 23:27:39 +0900

    Add files via upload

---
+++ js/portfolio.js
@@ -0,0 +1,42 @@
+$(function(){
+    $(window).scroll(function(){
+        let scrollPosition = $(window).scrollTop();
+        
+        let me = $('#me').offset().top;
+        let skill = $('#skill').offset().top;
+        let connect = $("#connect").offset().top;
+
+        if(scrollPosition >= me&&scrollPosition < skill){
+            $('header > ul > li:nth-child(2)').css('color', 'blue');
+            $('header > ul > li:nth-child(3)').css('color', 'black');
+        }else if(scrollPosition >= skill&&scrollPosition < connect){
+            $('header > ul > li:nth-child(2)').css('color', 'black');
+            $('header > ul > li:nth-child(3)').css('color', 'blue');
+        }else if(scrollPosition >= connect){
+            $("header > ul > li:nth-child(2)").css("color", "black");
+            $("header > ul > li:nth-child(3)").css("color", "black");
+            $("header > ul > li:nth-child(4)").css("color", "blue");
+        }else{
+            $("header > ul > li:nth-child(2)").css("color", "black");
+            $("header > ul > li:nth-child(3)").css("color", "black");
+            $("header > ul > li:nth-child(4)").css("color", "black");
+        }
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
