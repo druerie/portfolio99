dacca4aa1e03a6d03ad649a5f3ed99a8f3d3f9f0
HeeJung <72288033+druerie@users.noreply.github.com>
2023-12-10 19:54:45 +0900

    Add files via upload

--- js/portfolio.js
+++ js/portfolio.js
@@ -4,21 +4,33 @@ $(function(){
         
         let me = $('#me').offset().top;
         let skill = $('#skill').offset().top;
+        let site = $('#site').offset().top;
         let connect = $("#connect").offset().top;
 
         if(scrollPosition >= me&&scrollPosition < skill){
             $('header > ul > li:nth-child(2)').css('color', 'blue');
             $('header > ul > li:nth-child(3)').css('color', 'black');
-        }else if(scrollPosition >= skill&&scrollPosition < connect){
+            $('header > ul > li:nth-child(4)').css('color', 'black');
+            $('header > ul > li:last-child').css('color', 'black')
+        }else if(scrollPosition >= skill&&scrollPosition < site){
             $('header > ul > li:nth-child(2)').css('color', 'black');
             $('header > ul > li:nth-child(3)').css('color', 'blue');
-        }else if(scrollPosition >= connect){
+            $('header > ul > li:nth-child(4)').css('color', 'black');
+            $('header > ul > li:last-child)').css('color', 'black');
+        }else if(scrollPosition >= site&&scrollPosition < connect){
             $("header > ul > li:nth-child(2)").css("color", "black");
             $("header > ul > li:nth-child(3)").css("color", "black");
+            $("header > ul > li:nth-child(4)").css("color", "blue");
+            $("header > ul > li:last-child").css("color", "black");
+        }else if(scrollPosition == connect&&scrollPosition){
+            $("header > ul > li:nth-child(2)").css("color", "black");
+            $("header > ul > li:nth-child(3)").css("color", "black");
+            $("header > ul > li:nth-child(4)").css("color", "black");
             $("header > ul > li:last-child").css("color", "blue");
         }else{
             $("header > ul > li:nth-child(2)").css("color", "black");
             $("header > ul > li:nth-child(3)").css("color", "black");
+            $("header > ul > li:nth-child(4)").css("color", "black");
             $("header > ul > li:last-child").css("color", "black");
         }
 
